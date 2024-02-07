0258b939cf2c4f49d6934a60ebaf47e0003e5a81
lecturer-fkn <158597424+lecturer-fkn@users.noreply.github.com>
2024-02-08 00:07:49 +0200

    Change output in Calculator.java

--- Week1/Practice1/SimpleCalculator/src/calctask/Calculator.java
+++ Week1/Practice1/SimpleCalculator/src/calctask/Calculator.java
@@ -12,10 +12,10 @@ public class Calculator {
         System.out.print("Enter the second number: ");
         int secondNumber = sc.nextInt();
 
-        System.out.print("Enter one character - the type of operation you want to perform (+, -, *, /, %): ");
+        System.out.println("Enter one character - the type of operation you want to perform (+, -, *, /, %): ");
         String operation = sc.next();
         int result = performOperation(firstNumber, secondNumber, operation);
-        System.out.println("Your answer is: " + result);
+        System.out.println(firstNumber + " " + operation + " " + second Number + " = " + result);
     }
 
     public static int performOperation(int firstOperand, int secondOperand, String operation)
