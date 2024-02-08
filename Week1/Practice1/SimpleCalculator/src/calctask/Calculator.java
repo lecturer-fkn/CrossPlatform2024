1c6362c8aaf1f60fa86aa37bb98c1e2f63c9bf2b
alex-sporov <alex.sporov@gmail.com>
2024-02-08 16:08:19 +0200

    New percent operation

--- Week1/Practice1/SimpleCalculator/src/calctask/Calculator.java
+++ Week1/Practice1/SimpleCalculator/src/calctask/Calculator.java
@@ -3,23 +3,25 @@ package calctask;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter the first operand: ");
         int firstNumber = sc.nextInt();
-        System.out.print("Enter the second number: ");
+        System.out.print("Enter the second operand: ");
         int secondNumber = sc.nextInt();
 
-        System.out.print("Input the type of operation (+, -, *, /, %): ");
+        System.out.print("Input the type of operation (+, -, *, /, %, #): ");
         String operation = sc.next();
         int result = performOperation(firstNumber, secondNumber, operation);
         System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
     }
 
-    public static int performOperation(int firstOperand, int secondOperand, String operation)
-    {
+    private static int percent(int value, int per) {
+        return (int)Math.round((double)value*per/100);
+    }
+
+    public static int performOperation(int firstOperand, int secondOperand, String operation) {
         int result = 0;
         switch (operation) {
             case "+":
@@ -36,6 +38,9 @@ public class Calculator {
                 break;
             case "/":
                 result = firstOperand / secondOperand;
+                break;
+            case "#":
+                result = percent(firstOperand, secondOperand);
                 break;
             default:
                 System.out.println("Invalid operation");
