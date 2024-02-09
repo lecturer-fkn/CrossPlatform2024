015cedff5ff0d8e8c2a46da70070a4b2529aa253
alex-sporov <alex.sporov@gmail.com>
2024-02-09 19:13:34 +0200

    Calculator v. 1.0

--- Week1/Practice1/SimpleCalculator/src/calctask/Calculator.java
+++ Week1/Practice1/SimpleCalculator/src/calctask/Calculator.java
@@ -22,50 +22,5 @@ public class Calculator {
         y = new DataItem(25, "%");
         Operation o = new Percent(x, y);
         System.out.println(x + " " + o + " " + y + " = " + o.calc());
-        /*
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the first operand: ");
-        int firstNumber = sc.nextInt();
-        System.out.print("Enter the second operand: ");
-        int secondNumber = sc.nextInt();
-
-        System.out.print("Input the type of operation (+, -, *, /, %, #): ");
-        String operation = sc.next();
-        int result = performOperation(firstNumber, secondNumber, operation);
-        System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
-        */
-    }
-
-    private static int percent(int value, int per) {
-        return (int)Math.round((double)value*per/100);
-    }
-
-    public static int performOperation(int firstOperand, int secondOperand, String operation) {
-        int result = 0;
-        switch (operation) {
-            case "+":
-                result = firstOperand + secondOperand;
-                break;
-            case "-":
-                result = firstOperand - secondOperand;
-                break;
-            case "*":
-                result = firstOperand * secondOperand;
-                break;
-            case "%":
-                result = firstOperand % secondOperand;
-                break;
-            case "/":
-                result = firstOperand / secondOperand;
-                break;
-            case "#":
-                result = percent(firstOperand, secondOperand);
-                break;
-            default:
-                System.out.println("Invalid operation");
-                break;
-        }
-        return result;
     }
 }
